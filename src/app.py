b13a49383c84ac6f2ee99514c72fc90ff0bd2d0d
Thibaud Colas <thibaudcolas@gmail.com>
2015-04-08 03:14:24 +1200

    Make app port configurable

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+import os
 import flask
 
 app = flask.Flask(__name__)
@@ -12,4 +13,5 @@ def hello():
     return 'hello'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
