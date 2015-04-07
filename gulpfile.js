9d75d203c91fc80d8077841bd7ae8072cf14b374
Thibaud Colas <thibaudcolas@gmail.com>
2015-04-08 02:04:06 +1200

    Add JavaScript linting

--- gulpfile.js
+++ gulpfile.js
@@ -7,24 +7,34 @@ var babelify = require('babelify');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
 var sass = require('gulp-sass');
+var eslint = require('gulp-eslint');
+var jscs = require('gulp-jscs');
 
 var reload = browserSync.reload;
 
 gulp.task('build:js', function() {
     return browserify({debug: true})
       .transform([babelify, reactify])
-      .require(require.resolve('./src/js/main.js'), {entry: true})
+      .require(require.resolve('src/js/main.js'), {entry: true})
       .bundle()
       .pipe(source('bundle.js'))
-      .pipe(gulp.dest('./src/static/js'))
+      .pipe(gulp.dest('src/static/js'))
       .pipe(reload({stream: true}));
 });
 
 gulp.task('build:css', function() {
-    return gulp.src('./src/sass/*.scss')
+    return gulp.src('src/sass/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('./src/static/css'))
+        .pipe(gulp.dest('src/static/css'))
         .pipe(reload({stream: true}));
+});
+
+
+gulp.task('lint:js', function() {
+    return gulp.src(['src/js/**/*.{js,jsx}'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(jscs());
 });
 
 gulp.task('watch', ['build:js', 'build:css'], function() {
