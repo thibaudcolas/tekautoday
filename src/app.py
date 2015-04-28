639356ecf1b19b8546b5a4691d13a34d4b46d583
Thibaud Colas <thibaudcolas@gmail.com>
2015-04-28 21:50:53 +1200

    Load records from JSON & expose them

--- src/app.py
+++ src/app.py
@@ -1,5 +1,15 @@
 import os
+import json
+import random
 import flask
+
+from hashlib import md5
+
+records = {}
+
+# Create a hash table of all records.
+for record in json.loads(open('data/records-2015.json').read())['records']:
+    records[md5(str(record['id']).encode('utf-8')).hexdigest()] = record
 
 app = flask.Flask(__name__)
 
@@ -13,6 +23,12 @@ def index():
 def hello():
     return 'hello'
 
+
+@app.route('/random')
+def random_record():
+    record_hash = random.choice(list(records.keys()))
+    return str(records[record_hash])
+
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
