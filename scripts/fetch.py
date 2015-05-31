c97c4157ec06871dc8ced82720221815ce552806
Thibaud Colas <thibaudcolas@gmail.com>
2015-05-31 18:14:58 +1200

    New version of fetching script

--- scripts/fetch.py
+++ scripts/fetch.py
@@ -1,13 +1,16 @@
 import os
-# import math
+import math
 import json
 
 from pprint import pprint
-from datetime import date
-from scripts.pydnz import Dnz
+from datetime import date, datetime
+from pydnz import Dnz
 
 dnz_api = Dnz(os.environ.get('DNZ_KEY'))
+
 YEAR_INTERVAL = 10
+TODAY = date.today()
+PAST_YEAR = TODAY.year - YEAR_INTERVAL
 
 
 def request_dnz_records(timespan, page):
@@ -32,36 +35,40 @@ def format_timespan(year1, year2):
 
 
 def fetch_timespan(timespan):
-    pprint('Fetching ' + timespan)
 
     first_result = request_dnz_records(timespan, 1)
-    store_results(first_result.records)
+    store_records(first_result.records)
 
-    pprint(first_result.result_count)
+    print('Fetching ' + timespan + ':')
+    print(str(first_result.result_count) + ' entries')
 
-    # iterations = math.ceil(first_result.result_count / 100)
-    iterations = 1
+    iterations = math.ceil(first_result.result_count / 100)
+    # iterations = 5
 
     # Subsequent requests.
     for i in range(2, iterations + 1):
-        records = request_dnz_records(i).records
-        store_results(records)
+        records = request_dnz_records(timespan, i).records
+        store_records(records)
         pprint(len(results))
 
 
-def store_results(records):
+def store_record(record):
+    for date_str in record['date']:
+        date_str = date_str[:10]
+        date = datetime.strptime(date_str, '%Y-%m-%d').date()
+
+        if date.year >= PAST_YEAR and date.year <= TODAY.year:
+            results[date_str] = record['id']
+
+
+def store_records(records):
     for record in records:
-        results.append({
-            'id': record['id'],
-            'date': record['date']
-        })
+        store_record(record)
 
 if __name__ == '__main__':
-    results = []
+    results = {}
 
-    present = date.today().year
-    past = present - YEAR_INTERVAL
-    years = [y for y in range(2005, 2006)]
+    years = [y for y in range(PAST_YEAR, TODAY.year)]
     timespans = [format_timespan(y, y + 1) for y in years]
 
     for timespan in timespans:
