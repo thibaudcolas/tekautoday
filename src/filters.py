9796af8659a74f6822fa619df3e3fb6a2a0b0045
Thibaud Colas <thibaudcolas@gmail.com>
2015-06-01 23:47:24 +1200

    Add Flask filter to retrieve links for all dates in calendar

--- src/filters.py
+++ src/filters.py
@@ -1,10 +1,20 @@
 import jinja2
 import flask
+import records
 
 blueprint = flask.Blueprint('filters', __name__)
 
 
 @jinja2.contextfilter
 @blueprint.app_template_filter()
-def day_link(context, value):
-    return 1
+def date_record(context, date):
+    try:
+        record = records.get_record_by_date(date)
+    except StopIteration:
+        record = {
+            'hash': 'nothing',
+            'date': '3320-05-04',
+            'id': 1337
+        }
+
+    return record
